cf3fb52207e9288491acf472c0e8601fd741c041
Rufus Sommer <96887640+Sufur08@users.noreply.github.com>
2026-05-27 11:35:08 +0200

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.kt
@@ -0,0 +1,47 @@
+class Solution {
+
+    fun encode(strs: List<String>): String {
+        if (strs.isEmpty()) return ""
+        val strBuilder = StringBuilder()
+        for (str in strs)
+        {
+            for (char in str)
+                when (char)
+                {
+                    '\\' -> strBuilder.append("\\\\")
+                    ';' -> strBuilder.append("\\;")
+                    else -> strBuilder.append(char)
+                }
+            strBuilder.append(';')
+        }
+        return strBuilder.toString()
+    }
+
+    fun decode(str: String): List<String> {
+        if (str.isEmpty()) return emptyList()
+        val res = mutableListOf<String>()
+        var i = 0
+        var current = ""
+        while (i < str.lastIndex)
+            when (str[i])
+            {
+                '\\' -> {
+                    current += str[i + 1]
+                    // next must be ';' or '\'
+                    i += 2
+                    continue
+                }
+                ';' -> {
+                    res += current
+                    current = ""
+                    i++
+                }
+                else -> {
+                    current += str[i]
+                    i++
+                }
+            }
+        return res + current
+    }
+
+}
