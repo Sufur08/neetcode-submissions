af27e93661d1a5ba01746cab865ec3bc72095ca3
Rufus Sommer <96887640+Sufur08@users.noreply.github.com>
2026-05-28 16:08:04 +0200

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        val columns = List(9) { HashSet<Char>(9) }
+        val boxes = List(9) { HashSet<Char>(9) }
+        val currentRow = HashSet<Char>(9)
+        for (i in 0 until 9)
+        {
+            val row = board[i]
+            for (j in 0 until 9)
+            {
+                if (!(row[j] == '.' || columns[j].add(row[j]) && boxes[i - (i % 3) + j / 3].add(row[j]) && currentRow.add(row[j]))) return false
+            }
+            currentRow.clear()
+        }
+        return true
+    }
+}
